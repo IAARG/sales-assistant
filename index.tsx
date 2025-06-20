bc0a6a58363948d343da1c1a4cf58dbdacd255f7
IAARG <Jocislei.antunes@gmail.com>
2025-06-20 16:31:30 -0300

    Create index.tsx

---
+++ index.tsx
@@ -0,0 +1,9 @@
+// pages/index.tsx
+export default function Home() {
+  return (
+    <div>
+      <h1>IAARG - Assistente de Vendas</h1>
+      <p>Bem-vindo ao seu assistente de vendas com IA em tempo real!</p>
+    </div>
+  );
+}
