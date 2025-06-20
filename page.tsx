f2f948c408fcca0dc3a814222ffdf6ec692af83e
IAARG <Jocislei.antunes@gmail.com>
2025-06-20 15:08:04 -0300

    Add files via upload

---
+++ page.tsx
@@ -0,0 +1,8 @@
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen items-center justify-center bg-gray-100 text-gray-800">
+      <h1 className="text-3xl font-bold">Bem-vindo ao Sales Assistant</h1>
+    </main>
+  );
+}
