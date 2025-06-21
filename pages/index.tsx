82dd3cf677ba1063c1521a4ec99d9866675f32b8
IAARG <Jocislei.antunes@gmail.com>
2025-06-21 12:34:18 -0300

    Delete pages directory

--- pages/index.tsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div style={{ padding: 20, fontFamily: 'Arial, sans-serif' }}>
-      <h1>Assistente de Vendas</h1>
-      <p>Seu assistente com IA em tempo real.</p>
-    </div>
-  );
-}
