445d8280ef7ab6a42491e3f6ec1cbe7e82b106e6
IAARG <Jocislei.antunes@gmail.com>
2025-06-21 14:29:40 -0300

    Delete page.tsx

--- page.tsx
+++
@@ -1,9 +0,0 @@
-
-export default function Home() {
-  return (
-    <main>
-      <h1>Bem-vindo ao IAARG - Assistente de Vendas</h1>
-      <p>Este é o início do seu aplicativo com App Router.</p>
-    </main>
-  );
-}
