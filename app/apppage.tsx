28dafee084dd4933287b95c295c8141f7f4540fa
IAARG <Jocislei.antunes@gmail.com>
2025-06-21 12:34:04 -0300

    Delete app directory

--- app/apppage.tsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <main style={{ padding: "2rem" }}>
-      <h1>Bem-vindo ao Assistente de Vendas</h1>
-      <p>Seu sistema de vendas com IA está online!</p>
-    </main>
-  );
-}
