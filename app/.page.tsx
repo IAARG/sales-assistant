0929e05f51d8aad2db66656e2be7daf29906ef4e
IAARG <Jocislei.antunes@gmail.com>
2025-06-21 14:22:32 -0300

    Delete app directory

--- app/.page.tsx
+++
@@ -1,8 +0,0 @@
-// app/page.tsx
-export default function HomePage() {
-  return (
-    <main className="min-h-screen flex items-center justify-center">
-      <h1 className="text-4xl font-bold">Bem-vindo ao Assistente de Vendas!</h1>
-    </main>
-  );
-}
