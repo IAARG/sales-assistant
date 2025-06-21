902bd00ec17c7f9a46507f03ea8a3d10d13d9ec8
IAARG <Jocislei.antunes@gmail.com>
2025-06-21 14:29:26 -0300

    Delete index.tsx

--- index.tsx
+++
@@ -1,9 +0,0 @@
-// pages/index.tsx
-export default function Home() {
-  return (
-    <div>
-      <h1>IAARG - Assistente de Vendas</h1>
-      <p>Bem-vindo ao seu assistente de vendas com IA em tempo real!</p>
-    </div>
-  );
-}
