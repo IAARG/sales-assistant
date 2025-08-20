285aac1e686b46bf64740cfa0da7650f3d0f6981
IAARG <Jocislei.antunes@gmail.com>
2025-08-20 11:34:25 -0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx}",   // pega tudo dentro de app/
+    "./components/**/*.{js,ts,jsx,tsx}", // caso tenha pasta de components
   ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-Renomear pasta de aplicativo para app
